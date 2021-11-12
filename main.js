dc3f95c264bf7b03d8c58987970e60d5e72a7669
ombasa <ombasaherman@gmail.com>
2021-11-12 11:58:59 +0300

    ui elements

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ let min = 1,
     max = 10,
     winningNum = 2,
     guessesLeft = 3;
+
+
+//UI elements
+const game = document.getElementById('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      minNum = document.querySelector('#guess-input'),
+      minNum = document.querySelector('.message');
